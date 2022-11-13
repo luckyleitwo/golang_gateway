3c3183e8345353f49a9f65149cbd6ce18adf16e8
luckyleitwo <trustme_lei@163.com>
2022-11-13 23:11:22 +0800

    注释

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 )
 
 func main() {
+	// 初始化 config 文件
 	utils.InitConfig()
+	// 初始化 MySql
 	utils.InitMySql()
 	r := router.Router()
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
